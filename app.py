7953a5478a88a452be2af1d9fb632e9f652012e9
Boyuan Liu <boyuanliu6@yahoo.com>
2024-11-15 18:49:38 -0800

    added unbelievable edge cases

--- app.py
+++ app.py
@@ -20,6 +20,13 @@ def start_is_the_end_adventure(path):
         return file_processing("start", path)
     except FileNotFoundError:
         abort(404)
+        
+@app.route('/ics-h32/pset5/edge/<path:path>')
+def start_is_the_end_adventure(path):
+    try:
+        return file_processing("start", path)
+    except FileNotFoundError:
+        abort(404)
 
 def file_processing(directory, path):
     with open(os.path.join(directory, path)) as f:
