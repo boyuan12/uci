081ba618d4c97f9305d6e7cdc843c1c2cece0248
Boyuan Liu <boyuanliu6@yahoo.com>
2024-11-15 18:07:59 -0800

    added more testcases for pset 5 of ics h32

--- app.py
+++ app.py
@@ -10,15 +10,24 @@ def main():
 @app.route('/ics-h32/pset5/roads/<path:path>')
 def roads_adventure(path):
     try:
-        with open(os.path.join("roads", path)) as f:
-            data = f.read()
-
-            if not request.headers.get("Accept"):
-                return data
-            else:
-                return f"<pre>{data}</pre>"
+        return file_processing("roads", path)
     except FileNotFoundError:
         abort(404)
+
+@app.route('/ics-h32/pset5/start/<path:path>')
+def start_is_the_end_adventure(path):
+    try:
+        return file_processing("start", path)
+    except FileNotFoundError:
+        abort(404)
+
+def file_processing(directory, path):
+    with open(os.path.join(directory, path)) as f:
+        data = f.read()
+        if not request.headers.get("Accept"):
+            return data
+        else:
+            return f"<pre>{data}</pre>"
 
 if __name__ == "__main__":
     app.run()
